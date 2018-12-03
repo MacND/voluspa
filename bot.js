100f4c56b79fca218fabcdd0b0f88fcd06d66d99
Mac <johnny187896@live.co.uk>
2018-12-03 13:49:13 +0000

    Timezone aware .next command, fixed scheduling in your own timezone (thanks Alexis)

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@ const config = require("./config.json");
 const { google } = require('googleapis');
 const Maria = require("mariasql");
 const key = require('./googleApiKey.json');
-const moment = require('moment');
-const momenttz = require('moment-timezone');
+const moment = require('moment-timezone');
+moment.locale('en-gb');
 
 // Client declarations
 const client = new Discord.Client({ disableEveryone: true });
@@ -29,6 +29,7 @@ const jwtClient = new google.auth.JWT(
 // Variable declarations
 var events;
 var registeredUsers;
+var schedule;
 
 // Functions
 function getUsers() {
@@ -49,8 +50,18 @@ function getEvents() {
     sqlClient.end();
 }
 
+function getSchedule() {
+    sqlClient.query('SELECT s.id, s.fireteamId, s.adminId, s.startTime, s.raidReportUrl, s.joinCode, e.shortCode, e.name, TIME_FORMAT(SEC_TO_TIME(e.avgLength), "%k:%i") as avgLength, e.minPower FROM schedule s INNER JOIN events e ON e.shortCode = s.eventShortCode;', (err, rows) => {
+        if (err) throw (err);
+        console.log('Pulled schedule from database.');
+        schedule = rows;
+    });
+    sqlClient.end();
+}
+
 getUsers();
 getEvents();
+getSchedule();
 
 client.on("ready", async () => {
     console.log(`Successfully connected to Discord`);
@@ -116,10 +127,10 @@ client.on("message", async message => {
                 console.log('The API returned an error: ' + err);
             }
 
-            if ((args[0]).includes('#') && momenttz.tz.zone(args[1]) && !(response.data.files.length)) {
+            if ((args[0]).includes('#') && moment.tz.zone(args[1]) && !(response.data.files.length)) {
                 let bnetId = args[0];
                 let timezone = args[1];
-                let discordId = (message.author.id);
+                let discordId = message.author.id;
                 let copyResponse, permsResponse;
                 let copyBody = { 'name': message.author.id };
                 let createBody = { 'role': 'writer', 'type': 'anyone' };
@@ -151,7 +162,7 @@ client.on("message", async message => {
                     console.log(permsResponse.data);
                     message.author.send(`Your schedule spreadhseet has been created and is accessible at https://docs.google.com/spreadsheets/d/${newFileId}.  Please keep this link private, as it is shared by URL with no other security.`);
 
-                    sqlClient.query('UPDATE users SET gsheeturl = :gsheetId WHERE discordId = :discordId;', { discordId: discordId, gsheeturl: newFileId }, (err, rows) => {
+                    sqlClient.query('UPDATE users SET gsheeturl = :gsheeturl WHERE discordId = :discordId;', { discordId: discordId, gsheeturl: newFileId }, (err, rows) => {
                         if (err)
                             throw (err);
                         console.log(rows);
@@ -191,10 +202,12 @@ client.on("message", async message => {
                         if (err)
                             throw (err);
                         console.log(rows);
-                        message.channel.send(`${message.author} updated timezone to ${args[0]}.`);
+                        message.react("✅");
                     });
                     sqlClient.end();
                     getUsers();
+                } else {
+                    message.channel.send(`Your timezone is already set to ${args[0]}`);
                 }
             } else {
                 message.channel.send('Invalid timezone supplied.');
@@ -213,7 +226,7 @@ client.on("message", async message => {
                 if (err)
                     throw (err);
                 console.log(rows);
-                message.channel.send(`${message.author} updated BNet ID to ${args[0]}`);
+                message.react("✅");
             });
             sqlClient.end();
             getUsers();
@@ -232,7 +245,6 @@ client.on("message", async message => {
         } else {
             message.channel.send(`User information for ${client.users.get(searchUserId).tag}\`\`\`BNet ID: ${user.bnetId}\nTimezone: ${user.timezoneLocale}\`\`\``);
         }
-
     }
 
 
@@ -244,30 +256,103 @@ client.on("message", async message => {
     }
 
 
-    /*
-    if (command === "suggest" && args[0]) {
-        sqlClient.query('INSERT INTO schedule (eventShortCode, admin) VALUES (:shortCode, :adminId)', { shortcode: args[0], adminId: message.author.id }, function (err, rows) {
-            if (err) throw (err);
-            console.log(rows);
-            message.channel.send(`Created event with ID ${rows[0].info.insertId}`);
-        });
-        sqlClient.end();
+    if (command === "make" && args[0]) {
+        let event = events.find(o => o.shortCode == args[0]);
+        if (event) {
+            sqlClient.query(`START TRANSACTION;
+                    INSERT INTO schedule (eventShortCode, adminId) VALUES (:shortCode, :adminId);
+                    SELECT @scheduleId:=LAST_INSERT_ID();
+                    INSERT INTO fireteamMembers(guardianId, fireteamId) VALUES(:adminId, @scheduleId);
+                    UPDATE schedule SET joinCode = CONCAT(eventShortCode, '-', id), fireteamId = @scheduleId WHERE id = @scheduleId;
+                    COMMIT;`,
+                { shortCode: args[0], adminId: message.author.id }, (err, rows) => {
+                    if (err) throw (err);
+                    console.log(rows);
+                    message.channel.send(`Created event with ID ${args[0]}-${rows[1].info.insertId}`);
+                });
+            sqlClient.end();
+            getSchedule();
+        }
     }
-    */
+
+
+    if (command === "schedule") {
+        if (args[0]) {
+            let scheduledEvent = schedule.find(o => o.joinCode == args[0]);
+            if (scheduledEvent && (scheduledEvent.adminId === message.author.id)) {
+                if (args[1] && args[2]) {
+                    let creator = registeredUsers.find(o => o.discordId == message.author.id);
+
+                    moment.tz.setDefault(creator.timezoneLocale);
+                    let suggestedDateTime = moment.tz(moment(args[2], 'HH:mm').day(args[1]), creator.timezoneLocale);
+                    moment.tz.setDefault();
+
+                    console.log(suggestedDateTime);
+
+                    if (suggestedDateTime < moment().tz(creator.timezoneLocale)) {
+                        message.channel.send('Suggested time is in the past.');
+                        suggestedDateTime.add(7, 'd');
+                    }
+
+                    sqlClient.query('UPDATE schedule SET startTime = :suggestedTime WHERE joinCode = :joinCode AND adminId = :userId;',
+                        { suggestedTime: suggestedDateTime.utc().format('YYYY-MM-DD HH:mm:ss'), joinCode: args[0], userId: message.author.id }, (err, rows) => {
+                            if (err) throw (err);
+                            console.log(rows);
+                            message.channel.send(`Set start time of ${args[0]} to ${suggestedDateTime.format('YYYY-MM-DD HH:mm')} UTC`);
+                        });
+                    sqlClient.end();
+
+                }
+            }
+        }
+    }
+
+
+    if (command === "join") {
+        if (args[0]) {
+            let scheduledEvent = schedule.find(o => o.joinCode == args[0]);
+            console.log(scheduledEvent);
+            if (scheduledEvent) {
+                sqlClient.query('INSERT INTO fireteamMembers (guardianId, fireteamId) VALUES (:guardianId, :fireteamId);',
+                    { guardianId: message.author.id, fireteamId: scheduledEvent.fireteamId }, (err, rows) => {
+                        if (err) throw (err);
+                        console.log(rows);
+                        message.channel.send(`${message.author} joined ${scheduledEvent.joinCode}`);
+                    });
+                sqlClient.end();
+            }
+        }
+    }
+
+
+    if (command === "leave") {
+        if (args[0]) {
+            let scheduledEvent = schedule.find(o => o.joinCode == args[0]);
+            if (scheduledEvent) {
+                console.log(scheduledEvent);
+                sqlClient.query('DELETE FROM fireteamMembers WHERE guardianId = :guardianId AND fireteamId = :fireteamId);',
+                    { guardianId: message.author.id, fireteamId: scheduledEvent.fireteamId }, (err, rows) => {
+                        if (err) throw (err);
+                        console.log(rows);
+                        message.channel.send(`${message.author} left ${scheduledEvent.joinCode}`);
+                    });
+                sqlClient.end();
+            }
+        }
+    }
+
+
+    if (command === "next") {
+        let creator = registeredUsers.find(o => o.discordId == message.author.id);
+        let messageString = "";
+        schedule.forEach((scheduleLine, index) => {
+            messageString += `${scheduleLine.name} - ${(scheduleLine.startTime ? moment(scheduleLine.startTime).tz(creator.timezoneLocale).format('YYYY-MM-DD HH:mm') : 'Not Set')} \n!join ${scheduleLine.joinCode} | Length: ${scheduleLine.avgLength} | Power: ${scheduleLine.minPower} \n\n`;
+        });
+
+        message.channel.send((messageString ? `\`\`\`${messageString.trim()}\`\`\`` : "No events scheduled."));
+    }
+
 
 });
 
 client.login(config.token);
-
-/*
-START TRANSACTION; INSERT INTO schedule (eventShortCode, adminId) VALUES (:shortCode, :creatorId);
-SELECT @scheduleId:=LAST_INSERT_ID();
-
-INSERT INTO fireteamMembers (guardianId, fireteamId) VALUES (:creatorId, @scheduleId);
-
-UPDATE schedule
-    SET joinCode = CONCAT(eventShortCode,'-',id),
-    SET fireteamId = @scheduleId;
-END;
-*/
-
