8ba64010a79ea1ef2e8021553e7f5d6b3cf65f11
Mac <johnny187896@live.co.uk>
2018-12-21 02:01:04 +0000

    added singular getEvent function

--- db/db.js
+++ db/db.js
@@ -13,6 +13,9 @@ const pool = mysql.createPool({
 }).promise();
 
 module.exports = {
+    
+    // Users table
+
     getUsers: async () => {
         try {
             let [rows, fields] = await pool.query('SELECT * FROM users;');
@@ -22,9 +25,50 @@ module.exports = {
         }
     },
 
-    getActivities: async () => {
+    postUser: async (discordId, bnetId, timezone) => {
         try {
-            let [rows, fields] = await pool.query('SELECT * FROM activities;');
+            let [rows, fields] = await pool.query('INSERT INTO users (discordId, bnetId, timezone) VALUES (:discordId, :bnetId, :timezone);', 
+                {   discordId: discordId, 
+                    bnetId: bnetId, 
+                    timezone: timezone 
+                });
+            return rows;
+        } catch (err) {
+            throw new Error(err);
+        }
+    },
+
+    putUserTimezone: async (timezone, discordId) => {
+        try {
+            let [rows, fields] = await pool.query('UPDATE users SET timezone = :tz WHERE discordId = :discordId;', 
+                {   discordId: discordId, 
+                    tz: timezone 
+                });
+            return rows;
+        } catch (err) {
+            throw new Error(err);
+        } 
+    },
+
+    putUserBnet: async (bnet, discordId) => {
+        try {
+            let [rows,fields] = await pool.query('UPDATE users SET bnetId = :bnet WHERE discordId = :discordId;', 
+                {   discordId: discordId, 
+                    bnet: bnet 
+                });
+            return rows;
+        } catch (err) {
+
+        }
+    },
+
+    // Events table
+
+    getEvent: async (id) => {
+        try {
+            let [rows, fields] = await pool.query('SELECT * FROM vSchedule WHERE id = :id;',
+                {   id: id
+                });
             return rows;
         } catch (err) {
             throw new Error(err);
@@ -40,22 +84,79 @@ module.exports = {
         }
     },
 
-    getFireteams: async () => {
+    postEvent: async (shortCode, adminId, openedTime) => {
         try {
-            let [rows, fields] = await pool.query('SELECT fireteamId, GROUP_CONCAT(guardianId) AS guardians FROM fireteamMembers GROUP BY fireteamId;');
+            let [rows, fields] = await pool.query(`
+                START TRANSACTION; 
+                INSERT INTO events (eventShortCode, adminId, openedTime) VALUES (:shortCode, :adminId, :openedTime);
+                SELECT @eventId:=LAST_INSERT_ID();
+                SELECT @activityCount:=(SELECT COUNT(eventShortCode) FROM events WHERE eventShortCode = :shortCode);
+                INSERT INTO fireteamMembers(guardianId, fireteamId) VALUES(:adminId, @eventId);
+                UPDATE events SET joinCode = CONCAT(eventShortCode, '-', @activityCount), fireteamId = @eventId WHERE id = @eventId;
+                COMMIT;`,
+                {   shortCode: shortCode, 
+                    adminId: adminId,
+                    openedTime: openedTime 
+                });
             return rows;
         } catch (err) {
             throw new Error(err);
         }
     },
 
-    putUser: async (discordId, bnetId, timezone) => {
+    putEventStartTime: async (startTime, joinCode, adminId) => {
         try {
-            let [rows, fields] = await pool.query('INSERT INTO users (discordId, bnetId, timezone) VALUES (:discordId, :bnetId, :timezone);', 
-                {   discordId: discordId, 
-                    bnetId: bnetId, 
-                    timezone: timezone 
+            let [rows, fields] = await pool.query('UPDATE events SET startTime = :startTime WHERE joinCode = :joinCode AND adminId = :adminId;',
+                {   startTime: startTime, 
+                    joinCode: joinCode, 
+                    adminId: adminId
                 });
+            return rows;
+        } catch (err) {
+            throw new Error(err);
+        }
+    },
+
+    putEventFinishTime: async (joinCode) => {
+        try {
+            var [rows, fields] = await pool.query(`UPDATE events SET finishTime = :finishTime WHERE joinCode = :joinCode`,
+                {   finishTime: moment.utc().format('YYYY-MM-DD HH:mm'), 
+                    joinCode: joinCode 
+                });
+            return rows;
+        } catch (err) {
+            throw new Error(err);
+        }
+    },
+
+    putEventRaidReport: async (joinCode, rrLink) => {
+        try {
+            var [rows, fields] = await pool.query(`UPDATE events SET raidReportUrl = :rr WHERE joinCode = :joinCode`,
+                {   rr: rrLink, 
+                    joinCode: joinCode
+                });
+            return rows;
+        } catch (err) {
+            throw new Error(err);
+        }
+    },
+    
+    // Activities table 
+
+    getActivities: async () => {
+        try {
+            let [rows, fields] = await pool.query('SELECT * FROM activities;');
+            return rows;
+        } catch (err) {
+            throw new Error(err);
+        }
+    },
+
+    // Fireteam table
+
+    getFireteams: async () => {
+        try {
+            let [rows, fields] = await pool.query('SELECT fireteamId, GROUP_CONCAT(guardianId) AS guardians FROM fireteamMembers GROUP BY fireteamId;');
             return rows;
         } catch (err) {
             throw new Error(err);
@@ -74,20 +175,12 @@ module.exports = {
         }
     },
 
-    putEvent: async (shortCode, adminId, openedTime) => {
+    deleteFireteam: async (discordId, fireteamId) => {
         try {
-            let [rows, fields] = await pool.query(`
-                START TRANSACTION; 
-                INSERT INTO events (eventShortCode, adminId, openedTime) VALUES (:shortCode, :adminId, :openedTime);
-                SELECT @eventId:=LAST_INSERT_ID();
-                SELECT @activityCount:=(SELECT COUNT(eventShortCode) FROM events WHERE eventShortCode = :shortCode);
-                INSERT INTO fireteamMembers(guardianId, fireteamId) VALUES(:adminId, @eventId);
-                UPDATE events SET joinCode = CONCAT(eventShortCode, '-', @activityCount), fireteamId = @eventId WHERE id = @eventId;
-                COMMIT;`,
-                {   shortCode: shortCode, 
-                    adminId: adminId,
-                    openedTime: openedTime 
-                });
+            var [rows, fields] = await pool.query('DELETE FROM fireteamMembers WHERE guardianId = :guardianId AND fireteamId = :fireteamId;', 
+            {   guardianId: discordId, 
+                fireteamId: fireteamId 
+            });
             return rows;
         } catch (err) {
             throw new Error(err);
