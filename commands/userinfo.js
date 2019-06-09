98dba2c6192d957c9343c679984d8269c99e87c9
MacND <johnny187896@live.co.uk>
2019-06-09 16:47:14 +0100

    Migrated userinfo command

---
+++ commands/userinfo.js
@@ -0,0 +1,19 @@
+exports.run = async (client, message, args) => {
+  let [rows, fields] = await client.dbpool.query('SELECT * FROM users;');
+  let users = rows;
+
+  try {
+    let searchUserId = (args[0] ? client.users.find(user => user.username.toLowerCase() === args[0].toLowerCase()).id : message.author.id);
+    let user = users.find(o => o.discordId === searchUserId);
+
+    if (!user) {
+      message.channel.send(`${client.users.get(searchUserId).username} is not registered.`);
+      message.channel.stopTyping();
+      return;
+    }
+
+    message.channel.send(`User information for ${client.users.get(searchUserId).username}:\`\`\`BNet ID: ${user.bnetId}\nTimezone: ${user.timezone}\nTwitch: ${(user.twitch ? user.twitch : 'Not set')}\nNotifications: ${(user.newEventNotification ? 'On' : 'Off')}\`\`\``);
+  } catch (err) {
+    throw new Error(err);
+  }
+}
