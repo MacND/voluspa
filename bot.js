a03fd29574c9ae705ae78f2ba37acc214c30e538
MacND <johnny187896@live.co.uk>
2019-06-09 12:52:13 +0100

    Update gitignore, change up config file handling, add config file examples, rename getpool to createDbPool

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
 const fs = require("fs");
-const dbpool = require('./utils/getpool.js');
+const dbpool = require('./utils/createDbPool.js');
 const moment = require('./utils/moment.js');
 
 const client = new Discord.Client({ disableEveryone: true });
-const config = require("./config.json");
+const config = require("./config/discord.json");
 client.config = config;
 
 fs.readdir("./events/", (err, files) => {
