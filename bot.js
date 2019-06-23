4cfd9d3da46d324d8484a75672df8b9c6cd1b480
MacND <johnny187896@live.co.uk>
2019-06-24 00:34:07 +0100

    We refactorin' the DB again bois

--- bot.js
+++ bot.js
@@ -3,11 +3,12 @@ global.__basedir = __dirname;
 const Discord = require('discord.js');
 const Enmap = require('enmap');
 const fs = require('fs');
-const pool = require(__basedir + '/utils/database/pool.js');
 
 const client = new Discord.Client({ disableEveryone: true });
 const config = require(__basedir + '/config/discord.json');
 client.config = config;
+
+client.db = require(__basedir + '/utils/database/db.js');
 
 fs.readdir(__basedir + '/events/', (err, files) => {
   if (err) return console.error(err);
@@ -28,19 +29,6 @@ fs.readdir(__basedir + '/commands/', (err, files) => {
     let commandName = file.split('.')[0];
     console.log(`Attempting to load command ${commandName}`);
     client.commands.set(commandName, props);
-  });
-});
-
-client.database = new Enmap();
-
-fs.readdir(__basedir + '/utils/database/queries/', (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith('.js')) return;
-    let props = require(__basedir + `/utils/database/queries/${file}`)(pool);
-    let queryName = file.split('.')[0];
-    console.log(`Attempting to load DB query ${queryName}`);
-    client.database.set(queryName, props);
   });
 });
 
---
+++ utils/database/events.js
@@ -0,0 +1,106 @@
+module.exports = pool => ({
+  get: async () => {
+    try {
+      let rows = await pool.query('SELECT * FROM events e INNER JOIN activities a ON a.id = e.activity_id');
+      return rows;
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+
+  getById: async (id) => {
+    try {
+      let rows = await pool.query('SELECT * FROM events e INNER JOIN activities a ON a.id = e.activity_id WHERE e.id = :id',
+        {
+          id
+        }
+      );
+      return rows;
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+  
+  getByJoinCode: async (joinCode) => {
+    try {
+      let rows = await pool.query('SELECT * FROM events e INNER JOIN activities a ON a.id = e.activity_id WHERE join_code = :joinCode;',
+        {
+          joinCode
+        }
+      );
+      return rows;
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+
+  post: async (creatorId, activityId, activityNickname, openedTime) => {
+    try {
+      let rows = await pool.query(
+        `
+        START TRANSACTION; 
+        INSERT INTO events (activity_id, opened_time) VALUES (:activityId, :openedTime);
+        SELECT @eventId:=LAST_INSERT_ID();
+        SELECT @activityCount:=(SELECT COUNT(activity_id) FROM events WHERE activity_id = :activity_id);
+        INSERT INTO fireteams(user_id, event_id, admin) VALUES(:creatorId, @eventId, true);
+        UPDATE events SET join_code = CONCAT(activityNickname, '-', @activityCount);
+        COMMIT;`,
+        {
+          creatorId,
+          activityId,
+          openedTime,
+          activityNickname
+        }
+      );
+      return rows;
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+
+  putStartTime: async (startTime, joinCode) => {
+    try {
+      let rows = await pool.query('UPDATE events SET start_time = :startTime WHERE join_code = :joinCode;',
+        {
+          startTime,
+          joinCode
+        }
+      );
+      return rows;
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+
+  putFinishTime: async (finishTime, joinCode) => {
+    try {
+      let rows = await pool.query('UPDATE events SET finish_time = :finishTime WHERE join_code = :joinCode',
+        {
+          finishTime,
+          joinCode
+        }
+      );
+      return rows;
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+  
+  putRaidReport: async (rrLink, joinCode) => {
+    try {
+      let [
+        rows,
+        fields
+      ] = await pool.query(
+        'UPDATE events SET raidReportUrl = :rrLink WHERE join_code = :joinCode',
+        {
+          rrLink,
+          joinCode
+        }
+      );
+      return rows;
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+});
