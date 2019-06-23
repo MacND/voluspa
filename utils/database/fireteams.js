cd0aebcd65461e47647d0fccdb9c7e0774433627
MacND <johnny187896@live.co.uk>
2019-06-24 00:51:37 +0100

    All tables now have a file with their commands.  Let The Pain Begin

---
+++ utils/database/fireteams.js
@@ -0,0 +1,52 @@
+module.exports = pool => ({
+  get: async () => {
+    try {
+      let rows = await pool.query('SELECT event_id, GROUP_CONCAT(user_id) AS guardians FROM fireteams GROUP BY event_id;');
+      return rows;
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+
+  put: async (userId, eventId) => {
+    try {
+      let rows = await pool.query('INSERT INTO fireteams (user_id, event_id) VALUES (:userId, :eventId);',
+        {
+          userId,
+          eventId
+        }
+      );
+      return rows;
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+
+  delete: async (eventId) => {
+    try {
+      let rows = await pool.query('DELETE FROM fireteams WHERE event_id = :eventId;',
+        {
+          eventId
+        }
+      );
+
+      return rows;
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+
+  deleteMember: async (userId, eventId) => {
+    try {
+      let rows = await pool.query('DELETE FROM fireteams WHERE user_id = :userId AND event_id = :eventId;',
+        {
+          userId,
+          eventId
+        }
+      );
+      return rows;
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+});
