944de6c448f0699316f889417ccad671034498da
MacND <johnny187896@live.co.uk>
2019-09-16 22:34:40 +0100

    Remove commented code, no need to await pingUsers

--- commands/cancel.js
+++ commands/cancel.js
@@ -23,8 +23,7 @@ module.exports = {
 
       await db.events.delete(event.id);
       await db.fireteams.delete(event.id);
-      //message.reply(`Users in event when cancelled: ${fireteam.discord_id.split(',')}`);
-      await notify.pingUsers(fireteam.discord_id.split(','), `The event ${event.join_code} has been cancelled.`);
+      notify.pingUsers(fireteam.discord_id.split(','), `The event ${event.join_code} has been cancelled.`);
     } catch (err) {
       throw new Error(err);
     }
