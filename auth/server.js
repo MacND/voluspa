9dadc2ca17e439a3a06f2654e8916868de090305
MacND <johnny187896@live.co.uk>
2019-09-26 00:31:33 +0100

    Begin testing OAuth registration with express

---
+++ auth/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(443, () => {
+  console.info('Running on port 443');
+});
+
+// Routes
+app.use('/discord', require('./discord'));
+
+app.use((err, req, res, next) => {
+  switch (err.message) {
+    case 'NoCodeProvided':
+      return res.status(400).send({
+        status: 'ERROR',
+        error: err.message,
+      });
+    default:
+      return res.status(500).send({
+        status: 'ERROR',
+        error: err.message,
+      });
+  }
+});
