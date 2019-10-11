c5a5b40b246d8fa604f1f11237b85d77617c02b5
MacND <johnny187896@live.co.uk>
2019-10-11 02:49:02 +0100

    Add logging for timer creation

--- events/ready.js
+++ events/ready.js
@@ -12,8 +12,11 @@ module.exports = async client => {
       let event = events[i];
       let fireteam = await db.fireteams.getByEventId(event.id);
       notify.pingUsersBeforeEvent(fireteam.discord_id.split(','), `In 10 minutes you are scheduled to take part in **${event.join_code}**.  Please proceed to orbit and join up with your fireteam.`, moment(event.start_time).utc(), event.join_code);
+      console.log(`Created timer for ${event.join_code}`);
     }
   } catch (err) {
     throw new Error(err);
   }
+
+  console.log('Voluspa is online.');
 };
