b37022debfdb1c7c5b74310ea1ef9ad44e58f36e
MacND <johnny187896@live.co.uk>
2019-10-13 23:53:46 +0100

    Update Node to 12.12.0, update DiscordJS to latest master build (12.0dev), rename project in package files

--- commands/shaxx.js
+++ commands/shaxx.js
@@ -3,20 +3,26 @@ const fs = require('fs');
 module.exports = {
   run: async (client, message, args) => {
     try {
-      if (!message.member.voiceChannel) {
+      if (!message.member.voice.channel) {
         return;
       }
 
       let files = fs.readdirSync(__basedir + '/utils/shaxx');
       let file = files[Math.floor(Math.random() * files.length)];
-      let connection = await message.member.voiceChannel.join();
+      let connection = await message.member.voice.channel.join();
       console.log(__basedir + '/utils/shaxx/' + file);
-      let dispatcher = connection.playFile(__basedir + '/utils/shaxx/' + file);
+      let dispatcher = connection.play(__basedir + '/utils/shaxx/' + file);
 
-      dispatcher.on('end', () => message.member.voiceChannel.leave());
+      dispatcher.on('start', () => {
+        connection.player.streamingData.pausedTime = 0;
+      });
+
+      dispatcher.on('end', () => {
+        message.member.voice.channel.leave();
+      });
 
     } catch (err) {
-      message.member.voiceChannel.leave();
+      message.member.voice.channel.leave();
       throw new Error(err);
     }
   },
