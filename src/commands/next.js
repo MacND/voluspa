816f10b1501e0b08470a55d3cef4b79e644ba48b
MacND <johnny187896@live.co.uk>
2020-06-11 01:29:50 +0100

    Fix events page and next command

--- src/commands/next.js
+++ src/commands/next.js
@@ -6,29 +6,19 @@ module.exports = {
     try {
       let user = await db.users.getByDiscordId(message.author.id);
       let messageString = '';
-      let filterUser;
-      let filter;
 
-      if (args[0]) {
-        filterUser = client.users.cache.find(user => user.username.toLowerCase() === args[0].toLowerCase());
-        if (!filterUser) {
-          return message.reply(`Couldn't find user ${args[0]}.`);
-        }
-        filter = `%${filterUser.id}%`;
-      }
-
-      let events = await db.events.getNext(filter, message.guild.id);
+      let events = await db.events.getNext(message.guild.id);
 
       for (let i = 0; i < events.length; i++) {
         let event = events[i];
         messageString += `**${event.join_code}**: \`${event.fireteam.split(',').length}/6 - ${(event.start_time ? `${moment(event.start_time).tz((user ? user.timezone : 'UTC')).format('MMM Do [@] HH:mm z')}` : 'Not scheduled')}${event.fireteam.split(',').includes(message.author.id) ? ' - Joined' : ''}${event.private ? ' - 🔒' : ''}\`${event.note ? `\n • \`${event.note}\``:''}\n`;
       }
 
-      message.channel.send((messageString ? `Upcoming events${(filterUser ? ` for ${filterUser.username}`:'')}:\n${messageString.trim()}` : 'No events scheduled.'));
+      message.channel.send((messageString ? `Upcoming events:\n${messageString.trim()}` : 'No events scheduled.'));
     } catch (err) {
       throw new Error(err);
     }
   },
 
-  help: 'Show the next 3 events.  You can include a Discord username as an argument to view events that user has joined.'
+  help: 'Show the next 3 events in this server.'
 };
