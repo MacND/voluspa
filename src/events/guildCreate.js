d2bcc8d2326c7a0273638df858fb9fc1dfb79aac
MacND <johnny187896@live.co.uk>
2020-06-11 05:08:43 +0100

    Add guildCreate event

---
+++ src/events/guildCreate.js
@@ -0,0 +1,5 @@
+module.exports = async (client, guild) => {
+  console.log(`Joined new guild: ${guild}`);
+  const channel = guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'));
+  return channel.send("Greetings Guardians, my name is Voluspa.  If you've never used me before, you can register by visiting https://voluspa.app and logging in.  You can view my commands by typing \`!help\`.")
+};
