6d25f38155cc6893d9984e106be0f093a005f491
Mac <johnny187896@live.co.uk>
2020-11-23 09:01:04 +0000

    Fix formatting

--- src/commands/next.js
+++ src/commands/next.js
@@ -16,8 +16,8 @@ module.exports = {
       for (let i = 0; i < events.length; i++) {
         let event = events[i];
         messageString += `**${event.join_code}**: \`${event.fireteam.split(',').length}/6 - ${(event.start_time ? `${moment(event.start_time).tz((user ? user.timezone : 'UTC')).format('MMM Do [@] HH:mm z')}` : 'Not scheduled')}${event.fireteam.split(',').includes(message.author.id) ? ' - Joined' : ''}${event.private ? ' - 🔒' : ''}\`${event.note ? `\n • \`${event.note}\``:''}\n`;
-        messageString += (user ? `\nView events across all your servers at <https://voluspa.app/events>` : `\nWant to see events in your own timezone, across all your servers?  Login at <https://voluspa.app/login> and set your timezone.`)
       }
+      messageString += (user ? `\nView events across all your servers at <https://voluspa.app/events>` : `\nWant to see events in your own timezone, across all your servers?  Login at <https://voluspa.app/login> and set your timezone.`);
 
       message.channel.send((messageString ? `Upcoming events:\n${messageString.trim()}` : `No events scheduled.${(user ? `  View events across all your servers at <https://voluspa.app/events>` : `  Want to see events in your own timezone, across all your servers?  Login at <https://voluspa.app/login> and set your timezone.`)}`));
     } catch (err) {
